1861646e66b0912cee9d003d0b11b6eeeb0dc1c8
Ivan Kalinin <i.kalinin@dexpa.io>
2022-12-26 20:07:17 +0100

    Minimize dict fuzzing corpus

--- fuzz/fuzz_targets/boc_rc_dict.rs
+++ fuzz/fuzz_targets/boc_rc_dict.rs
@@ -1,14 +1,16 @@
 #![no_main]
-use libfuzzer_sys::fuzz_target;
+use libfuzzer_sys::{fuzz_target, Corpus};
 
 use everscale_types::cell::Load;
 use everscale_types::dict::HashmapE;
 use everscale_types::{Boc, RcCellFamily};
 
-fuzz_target!(|data: &[u8]| {
+fuzz_target!(|data: &[u8]| -> Corpus {
     if let Ok(cell) = Boc::<RcCellFamily>::decode(data) {
         if let Some(map) = HashmapE::<RcCellFamily, 32>::load_from(&mut cell.as_slice()) {
             _ = map.iter().count();
+            return Corpus::Keep;
         }
     }
+    Corpus::Reject
 });
