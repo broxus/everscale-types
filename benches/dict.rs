8610ead30957eeae105b90c252822775a803eb3d
Vladimir Petrzhikovskii <hello@0xdeafbeef.dev>
2024-10-12 15:51:35 +0200

    chore: make benchmarks more reproducible

--- benches/dict.rs
+++ benches/dict.rs
@@ -1,10 +1,10 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use everscale_types::cell::*;
-use everscale_types::dict::*;
+use everscale_types::{cell::*, dict::*};
+use iai_callgrind::{library_benchmark, library_benchmark_group, main};
 use rand::distributions::{Distribution, Standard};
 use rand::{Rng, SeedableRng};
+use std::hint::black_box;
 
-fn build_dict_impl<K, V>(id: BenchmarkId, num_elements: usize, c: &mut Criterion)
+fn build_dict<K, V>(num_elements: usize) -> Dict<K, V>
 where
     Standard: Distribution<K> + Distribution<V>,
     K: Store + DictKey,
@@ -12,57 +12,24 @@ where
 {
     let mut rng = rand_xorshift::XorShiftRng::from_seed([0u8; 16]);
 
-    let values = (0..num_elements)
-        .map(|_| (rng.gen::<K>(), rng.gen::<V>()))
-        .collect::<Vec<_>>();
-
-    c.bench_with_input(id, &values, |b, values| {
-        b.iter(|| {
-            let mut result = Dict::<K, V>::new();
-            for (key, value) in values {
-                result.set(key, value).unwrap();
-            }
-            black_box(result);
-        });
-    });
-}
-
-fn build_dict_group(c: &mut Criterion) {
-    macro_rules! decl_dict_benches {
-        ($({ $n:literal, $k:ty, $v:ident }),*$(,)?) => {
-            $({
-                let id = BenchmarkId::new(
-                    "build_dict",
-                    format!(
-                        "size={}; key={}; value={}",
-                        $n, stringify!($k), stringify!($v)
-                    )
-                );
-                build_dict_impl::<$k, $v>(id, $n, c);
-            });*
-        };
+    let mut result = Dict::<K, V>::new();
+    for _ in 0..num_elements {
+        let key = rng.gen::<K>();
+        let value = rng.gen::<V>();
+        result.set(key, value).unwrap();
     }
-
-    decl_dict_benches![
-        { 10, u8, u64 },
-        { 256, u8, u64 },
-
-        { 10, u16, u64 },
-        { 100, u16, u64 },
-        { 256, u16, u64 },
-        { 10000, u16, u64 },
-
-        { 10, u32, u64 },
-        { 100, u32, u64 },
-        { 1000, u32, u64 },
-        { 100000, u32, u64 },
-
-        { 10, u64, u64 },
-        { 100, u64, u64 },
-        { 1000, u64, u64 },
-        { 100000, u64, u64 },
-    ];
+    result
 }
 
-criterion_group!(build_dict, build_dict_group);
-criterion_main!(build_dict);
+#[library_benchmark]
+#[bench::small(10)]
+#[bench::medium(100)]
+#[bench::large(1000)]
+#[bench::xlarge(10000)]
+fn bench_build_dict_u64_u64(num_elements: usize) -> Dict<u64, u64> {
+    black_box(build_dict(num_elements))
+}
+
+library_benchmark_group!(name = build_dict; benchmarks = bench_build_dict_u64_u64);
+
+main!(library_benchmark_groups = build_dict);
--- benches/usage_cell.rs
+++ benches/usage_cell.rs
@@ -1,50 +1,51 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use everscale_types::cell::RefsIter;
 use everscale_types::prelude::*;
+use iai_callgrind::{library_benchmark, library_benchmark_group, main};
+use std::collections::HashSet;
+use std::hint::black_box;
 
 const BOC: &str = "te6ccgECCAEAAWQAAnPP9noJKCEBL3oZerOiIcNghuL96V3wIcuYOWQdvNC+2fqCEIJDQAAAAAAAAAAAAAAAAZa8xB6QABNAAgEAUO3QlUyMI4dEepUMw3Ou6oSqq8+1lyHkjOGFK6DAn6TXAAAAAAAAAAABFP8A9KQT9LzyyAsDAgEgBwQC5vJx1wEBwADyeoMI1xjtRNCDB9cB1ws/yPgozxYjzxbJ+QADcdcBAcMAmoMH1wFRE7ry4GTegEDXAYAg1wGAINcBVBZ1+RDyqPgju/J5Zr74I4EHCKCBA+ioUiC8sfJ0AiCCEEzuZGy64w8ByMv/yz/J7VQGBQA+ghAWnj4Ruo4R+AACkyDXSpd41wHUAvsA6NGTMvI84gCYMALXTND6QIMG1wFx1wF41wHXTPgAcIAQBKoCFLHIywVQBc8WUAP6AstpItAhzzEh10mghAm5mDNwAcsAWM8WlzBxAcsAEsziyQH7AAAE0jA=";
 
-fn traverse_cell_ordinary(c: &mut Criterion) {
+#[library_benchmark]
+fn traverse_cell_ordinary() {
     let cell = Boc::decode_base64(BOC).unwrap();
 
-    c.bench_function("traverse cell ordinary", |b| {
-        b.iter(|| {
-            let mut visitor = Visitor::default();
-            black_box(visitor.add_cell(cell.as_ref()));
-        })
-    });
+    let mut visitor = Visitor::new();
+    black_box(visitor.add_cell(cell.as_ref()));
 }
 
-fn traverse_cell_storage_cell(c: &mut Criterion) {
+#[library_benchmark]
+fn traverse_cell_storage_cell() {
     let cell = Boc::decode_base64(BOC).unwrap();
     let usage_tree = UsageTree::new(UsageTreeMode::OnDataAccess);
     let cell = usage_tree.track(&cell);
 
-    c.bench_function("traverse cell usage tree", |b| {
-        b.iter(|| {
-            let mut visitor = Visitor::default();
-            black_box(visitor.add_cell(cell.as_ref()));
-        })
-    });
+    let mut visitor = Visitor::new();
+    black_box(visitor.add_cell(cell.as_ref()));
 }
 
-fn traverse_cell_storage_cell_with_capacity(c: &mut Criterion) {
+#[library_benchmark]
+fn traverse_cell_storage_cell_with_capacity() {
     let cell = Boc::decode_base64(BOC).unwrap();
     let usage_tree = UsageTree::with_mode_and_capacity(UsageTreeMode::OnDataAccess, 100);
     let cell = usage_tree.track(&cell);
 
-    c.bench_function("traverse cell usage tree with capacity", |b| {
-        b.iter(|| {
-            let mut visitor = Visitor::default();
-            black_box(visitor.add_cell(cell.as_ref()));
-        })
-    });
+    let mut visitor = Visitor::new();
+    black_box(visitor.add_cell(cell.as_ref()));
 }
 
-#[derive(Default)]
 struct Visitor<'a> {
     visited: ahash::HashSet<&'a HashBytes>,
     stack: Vec<RefsIter<'a>>,
+}
+
+impl<'a> Visitor<'a> {
+    fn new() -> Self {
+        Self {
+            visited: HashSet::with_hasher(ahash::RandomState::with_seed(0)),
+            stack: Vec::new(),
+        }
+    }
 }
 
 impl<'a> Visitor<'a> {
@@ -85,10 +86,9 @@ impl<'a> Visitor<'a> {
     }
 }
 
-criterion_group!(
-    benches,
-    traverse_cell_ordinary,
-    traverse_cell_storage_cell,
-    traverse_cell_storage_cell_with_capacity
+library_benchmark_group!(
+    name = traverse_cell;
+    benchmarks = traverse_cell_ordinary, traverse_cell_storage_cell, traverse_cell_storage_cell_with_capacity
 );
-criterion_main!(benches);
+
+main!(library_benchmark_groups = traverse_cell);
