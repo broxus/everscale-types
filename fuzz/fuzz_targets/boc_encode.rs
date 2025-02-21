cac539d94380907c053af7147a14a0ef53735a1e
Vladimir Petrzhikovskii <hello@0xdeafbeef.dev>
2025-02-21 18:53:52 +0100

    chore: make boc_encode more exhaustive

--- fuzz/fuzz_targets/boc_encode.rs
+++ fuzz/fuzz_targets/boc_encode.rs
@@ -4,5 +4,8 @@ use libfuzzer_sys::fuzz_target;
 use everscale_types::prelude::{Boc, Cell};
 
 fuzz_target!(|cell: Cell| {
-    _ = Boc::encode(cell);
+    let bytes = Boc::encode(&cell);
+    let decoded = Boc::decode(&bytes).unwrap();
+
+    assert_eq!(cell, decoded);
 });
