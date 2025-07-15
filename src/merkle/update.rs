cc83aae37b9ffce5b647e0c2a41060739f83f678
Alexey Pashinov <pashinov93@gmail.com>
2025-07-15 19:51:20 +0300

    increase split depth

--- src/merkle/update.rs
+++ src/merkle/update.rs
@@ -292,7 +292,7 @@ impl MerkleUpdate {
         old: &Cell,
         context: &(dyn CellContext + Send + Sync),
     ) -> Result<Cell, Error> {
-        const SPLIT_DEPTH: u16 = 4;
+        const SPLIT_DEPTH: u16 = 5;
 
         if old.as_ref().repr_hash() != &self.old_hash {
             return Err(Error::InvalidData);
@@ -784,7 +784,7 @@ impl MerkleUpdate {
 
     #[cfg(all(feature = "rayon", feature = "sync"))]
     fn par_find_old_cells(&self) -> dashmap::DashSet<HashBytes, ahash::RandomState> {
-        const SPLIT_DEPTH: u16 = 4;
+        const SPLIT_DEPTH: u16 = 5;
 
         let visited = Default::default();
         let old_cells = Default::default();
